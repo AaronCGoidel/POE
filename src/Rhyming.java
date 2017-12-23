76f15bc274cb039e0448a2cd919bfb82e38a5ce1
AaronCGoidel <agoidel2019@mka.org>
2017-12-23 16:00:28 -0500

    check if two words rhyme

--- src/Rhyming.java
+++ src/Rhyming.java
@@ -12,7 +12,7 @@ public class Rhyming
         String[] inEnding = Arrays.copyOfRange(inPronunciation,
                 inPronunciation.length - depth, inPronunciation.length);
 
-        System.out.println(Arrays.toString(inEnding));
+        //System.out.println(Arrays.toString(inEnding));
 
         for(String key : dict.keySet()){
             String[] checkPronunciation = dict.get(key);
@@ -20,18 +20,24 @@ public class Rhyming
             try{
                 String[] checkEnding = Arrays.copyOfRange(checkPronunciation,
                         checkPronunciation.length - depth, checkPronunciation.length);
-                System.out.println(Arrays.toString(checkEnding));
+                //System.out.println(Arrays.toString(checkEnding));
 
                 if(Arrays.equals(inEnding, checkEnding)){
-                    System.out.println(key);
+                    //System.out.println(key);
                     matches.add(key);
                 }
             }catch(ArrayIndexOutOfBoundsException oops){
-                System.out.println("word too short");
+                //System.out.println("word too short");
             }
         }
 
-        System.out.println(matches.size());
+        //System.out.println(matches.size());
         return matches;
+    }
+
+    public static boolean isRhyme(String base, String compare, int depth, Map<String, String[]> dict)
+    {
+        ArrayList rhymes = findRhymes(base, depth, dict);
+        return rhymes.contains(compare);
     }
 }
