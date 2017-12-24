90e998f84ea45291d4cb923b8d6ebd64b03f3aa8
AaronCGoidel <agoidel2019@mka.org>
2017-12-23 23:53:10 -0500

    Better rhyme detector(I think) Instead of generating a list of rhymes and checking it we now just compare two words directly

---
+++ src/Rhymer.java
@@ -0,0 +1,52 @@
+import dictionary.SoundClass;
+
+import java.util.Map;
+
+public class Rhymer
+{
+    Map<String, String[]> dict;
+    Map<String, SoundClass> phonetics;
+    int depth;
+
+    /*
+    Constructor with default depth
+     */
+    public Rhymer(Map<String, String[]> dict, Map<String, SoundClass> phonetics)
+    {
+        this.dict = dict;
+        this.phonetics = phonetics;
+        this.depth = 3;
+    }
+
+    public Rhymer(Map<String, String[]> dict, Map<String, SoundClass> phonetics, int depth)
+    {
+        this.dict = dict;
+        this.phonetics = phonetics;
+        this.depth = depth;
+    }
+
+    public boolean isRhyme(String base, String compare)
+    {
+        // get phonetic breakdowns from dictionary
+        String[] pronunciationOne = dict.get(base);
+        String[] pronunciationTwo = dict.get(compare);
+
+        // shorten depth of search for small words
+        int vowelCount = 0;
+        for(String sound : pronunciationOne){ // iterate over sounds
+            if(phonetics.get(sound.length() < 2 ? sound : sound.substring(0, 2)) == SoundClass.vowel){
+                vowelCount++; // count the number of vowels found
+            }
+        }
+        if(vowelCount < 3){
+            depth = 2;
+        }
+        for(int i = pronunciationOne.length - 1, j = pronunciationTwo.length - 1;
+            i > pronunciationOne.length - depth && j > pronunciationTwo.length - depth; i--, j--){
+            if(!pronunciationOne[i].equals(pronunciationTwo[j])){ // compare the pronunciations from the end to the beginning in the range
+                return false;
+            }
+        }
+        return true;
+    }
+}
