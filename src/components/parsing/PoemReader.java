a32da98ae882844b013d8a6b96175356fa38fd39
AaronCGoidel <agoidel2019@mka.org>
2017-12-25 23:00:36 -0500

    clarifications and comments

--- src/components/parsing/PoemReader.java
+++ src/components/parsing/PoemReader.java
@@ -25,15 +25,21 @@ public class PoemReader
         return text.toArray(new String[0]);
     }
 
+    /**
+     * Takes in the lines of a poem to clean and tokenize the text
+     * Removes punctuation and blank lines and splits the lines up by word
+     * @param rawText String[] the text taken in by readPoem()
+     * @return String[][] An array which represents each line made up of an array of each word in the line
+     */
     public static String[][] cleanRaw(String[] rawText)
     {
         List<String[]> clean = new ArrayList<>();
         for(String line : rawText){
-            if(!line.equals("")){
-                clean.add(line.replaceAll("[.,;:?!]", "").split(" "));
+            if(!line.equals("")){ // don't include blank lines
+                clean.add(line.replaceAll("[.,;:?!]", "").split(" ")); // filter out punctuation
             }
         }
 
-        return clean.toArray(new String[0][0]);
+        return clean.toArray(new String[0][0]); // convert from List to Array
     }
 }
--- src/components/rhyming/RhymeScheme.java
+++ src/components/rhyming/RhymeScheme.java
@@ -13,24 +13,29 @@ public class RhymeScheme
     {
         this.text = text;
         this.pattern = new StringBuilder(text.length);
-        for(int i = 0; i < text.length; i++){
+        for(int i = 0; i < text.length; i++){ // make default pattern all dashes (dashes denote non-rhymes)
             pattern.append("-");
         }
         this.rhymer = rhymer;
     }
 
-    public String calculatePattern()
+    /**
+     * Takes the text of a poem and returns the actual rhyming pattern
+     * Takes the last word from each line and finds rhyming lines later in the poem
+     * @return String The rhyming pattern of the poem
+     */
+    private String calculatePattern()
     {
-        char currentSymbol = 96;
+        char currentSymbol = 96; // represent first rhyming sound with the letter a
         for(int line = 0; line < text.length; line++){
-            boolean found = false;
+            boolean found = false; // to keep track of whether to switch to next letter
             for(int next = line + 1; next < text.length; next++){
                 if(pattern.charAt(next) == '-'){
                     String[] baseLine = text[line];
                     String[] nextLine = text[next];
 
                     if(rhymer.isRhyme(baseLine[baseLine.length - 1], nextLine[nextLine.length - 1])){
-                        if(!found){
+                        if(!found){ // if this rhyme is new then increase to next letter
                             found = true;
                             currentSymbol++;
                         }
@@ -43,15 +48,22 @@ public class RhymeScheme
         return pattern.toString();
     }
 
+    /**
+     * Returns the best estimate of the type of rhyme scheme based on the pattern of the rhyming lines
+     * Takes the Levenshtein distance between each rhyme scheme and the pattern from the poem
+     * Selects the closest match as the form of the poem
+     * @return RhymePattern The closest form to the pattern found in the poem
+     */
     public RhymePattern estimateScheme()
     {
         String actualPattern = calculatePattern();
 
-        RhymePattern best = RhymePattern.NONE;
-        int min = Integer.MAX_VALUE;
-        for(RhymePattern rhymePattern : RhymePattern.values()){
+        RhymePattern best = RhymePattern.NONE; // defaults to no scheme
+        int min = Integer.MAX_VALUE; // set the initial minimum distance to something absurdly high
+        for(RhymePattern rhymePattern : RhymePattern.values()){ // iterate through the rhyme schemes
+            // calculate difference between actual pattern from the poem and the pattern of the rhyme scheme
             int d = Utils.levenshteinDistance(actualPattern, rhymePattern.pattern);
-            if(d < min){
+            if(d < min){ // check if this distance is closer than the previous best
                 min = d;
                 best = rhymePattern;
             }
